82545b10bd5e5539d9973f7ad8ae891961434580
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-11-10 16:17:05 +0700

    install router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const version = "1.0.0"
@@ -17,8 +18,13 @@ type config struct {
 
 type AppStatus struct {
 	Status       string `json:"status"`
-	Environtment string	`json:"environtment"`
-	Version      string	`json:"version"`
+	Environtment string `json:"environtment"`
+	Version      string `json:"version"`
+}
+
+type application struct {
+	config config
+	logger *log.Logger
 }
 
 func main() {
@@ -29,26 +35,24 @@ func main() {
 	flag.StringVar(&cfg.env, "env", "development", "Application environtment (development|production)")
 	flag.Parse()
 
-	fmt.Println("Running")
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		currentStatus := AppStatus{
-			Status:       "Available",
-			Environtment: cfg.env,
-			Version:      version,
-		}
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
 
-		js, err := json.MarshalIndent(currentStatus, "", "\t")
-		if err != nil {
-			log.Println(err)
-		}
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(js)
-	})
+	logger.Println("Starting server on port ", cfg.port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Println(err)
 	}
